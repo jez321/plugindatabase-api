b34642e578874009bc09883204cd552686b6a97d
root <root@JJDesktop.localdomain>
2020-02-11 17:09:53 +0900

    add plugin manufancturer link url to deals and plugins GET

--- api/controllers/deals.js
+++ api/controllers/deals.js
@@ -2,20 +2,20 @@ const db = require('../db')
 const sortableColumns = ['name', 'company', 'category', 'price', 'description', 'start_date', 'end_date', 'created', 'updated'];
 
 const getDeals = async function (req, res) {
-    const search = req.query.search;
-    const searchField = search && search.length > 0 ? `WHERE LOWER(plugin.name) LIKE LOWER($1)` : '';
-    const params = searchField === '' ? [] : ['%' + search + '%'];
+  const search = req.query.search;
+  const searchField = search && search.length > 0 ? `WHERE LOWER(plugin.name) LIKE LOWER($1)` : '';
+  const params = searchField === '' ? [] : ['%' + search + '%'];
 
-    // sanitize sortby
-    let sortby = req.query.sortby;
-    if (!sortableColumns.includes(sortby)) {
-        sortby = 'added'
-    }
+  // sanitize sortby
+  let sortby = req.query.sortby;
+  if (!sortableColumns.includes(sortby)) {
+    sortby = 'added'
+  }
 
-    // sanitize sortdir
-    const sortdir = req.query.sortdir === 'desc' ? 'desc' : 'asc';
+  // sanitize sortdir
+  const sortdir = req.query.sortdir === 'desc' ? 'desc' : 'asc';
 
-    const data = await db.pool.query(`SELECT d.id_deal, d.id_plugin, plugin.name as name, company.name as company, category.name as category, d.price, d.description, d.start_date, d.end_date, d.created as added, json_build_object('title', link.title, 'url', link.url) AS link, t.tags, d.created, d.updated
+  const data = await db.pool.query(`SELECT d.id_deal, d.id_plugin, plugin.name as name, company.name as company, category.name as category, d.price, d.description, d.start_date, d.end_date, d.created as added, json_build_object('title', link.title, 'url', link.url) AS link, t.tags, d.created, d.updated, p_link.url
                 FROM deal d
                 JOIN plugin ON d.id_plugin = plugin.id_plugin
                 JOIN company ON company.id_company = plugin.id_company
@@ -27,17 +27,19 @@ const getDeals = async function (req, res) {
                     GROUP  BY deal_tag.id_deal) t ON t.id_deal = d.id_deal
                 JOIN plugin_category on plugin_category.id_plugin = plugin.id_plugin
                 JOIN category on plugin_category.id_category = category.id_category
+                JOIN plugin_link on plugin_link.id_plugin = d.id_plugin
+                JOIN link as p_link on plugin_link.id_link = p_link.id_link
                 ${searchField}
                 ORDER BY ${sortby} ${sortdir}
     ;`, params)
-    const lowestPrices = await db.pool.query(`SELECT * FROM lowest_prices;`)
-    data.rows.forEach(r => {
-        r.lowestPrice = lowestPrices.rows.find(lp => {
-            return lp.id_plugin === r.id_plugin
-        }).price
-    })
-    res.json(data.rows);
-    // pool.end()
+  const lowestPrices = await db.pool.query(`SELECT * FROM lowest_prices;`)
+  data.rows.forEach(r => {
+    r.lowestPrice = lowestPrices.rows.find(lp => {
+      return lp.id_plugin === r.id_plugin
+    }).price
+  })
+  res.json(data.rows);
+  // pool.end()
 }
 
 exports.getDeals = getDeals
--- api/controllers/plugins.js
+++ api/controllers/plugins.js
@@ -2,32 +2,34 @@ const db = require('../db')
 const sortableColumns = ['name', 'plugin.created', 'plugin.updated'];
 
 const getPlugins = function (req, res) {
-    const search = req.query.search;
-    const searchField = search && search.length > 0 ? `WHERE LOWER(plugin.name) LIKE LOWER($1)` : '';
-    const params = searchField === '' ? [] : ['%' + search + '%'];
+  const search = req.query.search;
+  const searchField = search && search.length > 0 ? `WHERE LOWER(plugin.name) LIKE LOWER($1)` : '';
+  const params = searchField === '' ? [] : ['%' + search + '%'];
 
-    // sanitize sortby
-    let sortby = req.query.sortby;
-    if (!sortableColumns.includes(sortby)) {
-        sortby = 'name'
-    }
+  // sanitize sortby
+  let sortby = req.query.sortby;
+  if (!sortableColumns.includes(sortby)) {
+    sortby = 'name'
+  }
 
-    // sanitize sortdir
-    const sortdir = req.query.sortdir === 'desc' ? 'desc' : 'asc';
+  // sanitize sortdir
+  const sortdir = req.query.sortdir === 'desc' ? 'desc' : 'asc';
 
-    db.pool.query(`SELECT plugin.id_plugin, plugin.name as name, category.name as category, plugin.created, plugin.updated, company.name as company
+  db.pool.query(`SELECT plugin.id_plugin, plugin.name as name, category.name as category, plugin.created, plugin.updated, company.name as company, link.url as url
                     FROM plugin
                     JOIN company ON company.id_company = plugin.id_company
                     JOIN plugin_category on plugin_category.id_plugin = plugin.id_plugin
                     JOIN category on plugin_category.id_category = category.id_category
+                    JOIN plugin_link on plugin_link.id_plugin = plugin.id_plugin
+                    JOIN link on plugin_link.id_link = link.id_link
                 ${searchField}
                 ORDER BY ${sortby} ${sortdir}
     ;`, params, (err, data) => {
-        if (err) {
-            console.log(err)
-        }
-        res.json(data.rows);
-        // pool.end()
-    })
+    if (err) {
+      console.log(err)
+    }
+    res.json(data.rows);
+    // pool.end()
+  })
 }
 exports.getPlugins = getPlugins
